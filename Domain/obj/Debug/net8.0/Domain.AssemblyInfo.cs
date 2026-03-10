91d849780cc64dfea017e002129d4a5fb012522b
Andre <andre.ats.profissional@gmail.com>
2026-03-10 15:28:55 -0300

    Ajustando factory admin

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+533d1e8bf97225b432d6e059b7e71b078731143b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98137a75e633000ce60ed3ab89daa4d2dc2f6db6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
