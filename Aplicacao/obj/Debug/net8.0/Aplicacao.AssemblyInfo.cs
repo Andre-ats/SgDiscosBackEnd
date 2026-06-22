e1eb128a24aa75e46c0c673e448dfa0e24095c8d
Andre <andre.ats.profissional@gmail.com>
2026-06-22 02:54:27 -0300

    Criando projeto api com swagger

--- Aplicacao/obj/Debug/net8.0/Aplicacao.AssemblyInfo.cs
+++ Aplicacao/obj/Debug/net8.0/Aplicacao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicacao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaa5843eefe31213c87670edf22d4e102d8ff816")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97907ae07b278462bee2fd973796dc1e291ca7e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicacao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicacao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Infraestrutura/obj/Debug/net8.0/Infraestrutura.AssemblyInfo.cs
+++ Infraestrutura/obj/Debug/net8.0/Infraestrutura.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infraestrutura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaa5843eefe31213c87670edf22d4e102d8ff816")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97907ae07b278462bee2fd973796dc1e291ca7e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infraestrutura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infraestrutura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
