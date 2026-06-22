d6568e32d017f613e5189b5e4addd32d7f56f438
Andre <andre.ats.profissional@gmail.com>
2026-06-22 01:27:07 -0300

    EFcore admin

---
+++ Infraestrutura/Repositorio/AdminRepositorio/EFCoreAdminRepository.cs
@@ -0,0 +1,44 @@
+using Domain.Entidade.AdminEntidade;
+using FluentResults;
+
+namespace Infraestrutura.Repositorio.AdminRepositorio;
+
+public class EFCoreAdminRepository : IAdminRepositorio
+{
+    private readonly DataBaseContext _dataBaseContext = null!;
+
+    public EFCoreAdminRepository(DataBaseContext context)
+    {
+        _dataBaseContext = context;
+    }
+    
+    public Result<bool> CriarAdmin(Admin admin)
+    {
+        bool jaExiste = _dataBaseContext.AdminsDB.Any(x => x.Email == admin.Email);
+
+        if (jaExiste)
+            return Result.Fail("Já existe um admin com este e-mail.");
+        
+        _dataBaseContext.AdminsDB.Add(admin);
+        
+        var salvou = _dataBaseContext.SaveChanges() > 0;
+
+        if (!salvou)
+            return Result.Fail("Não foi possível criar o admin.");
+
+        return Result.Ok(true);
+    }
+
+    public Result<Admin> GetAdminLogin(string email, string senha)
+    {
+        var admin = _dataBaseContext.AdminsDB.FirstOrDefault(x => x.Email == email);
+        
+        if (admin is null)
+            return Result.Fail("E-mail não encontrado");
+
+        if (admin.Senha != senha)
+            return Result.Fail("Senha incorreta");
+
+        return Result.Ok(admin);
+    }
+}
--- Infraestrutura/Repositorio/AdminRepositorio/IAdminRepositorio.cs
+++ Infraestrutura/Repositorio/AdminRepositorio/IAdminRepositorio.cs
@@ -6,5 +6,5 @@ namespace Infraestrutura.Repositorio.AdminRepositorio;
 public interface IAdminRepositorio
 {
     public Result<bool> CriarAdmin(Admin admin);
-    public Result<Admin> GetAdminLogin(string login, string senha);
+    public Result<Admin> GetAdminLogin(string email, string senha);
 }
